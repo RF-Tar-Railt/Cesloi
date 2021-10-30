3e01086869f37b5f51268e1e1da86ccf346f2659
RF-Tar-Railt <42648639+RF-Tar-Railt@users.noreply.github.com>
2021-10-30 19:47:52 +0800

    Add files via upload

---
+++ cesloi/__init__.py
@@ -0,0 +1,10 @@
+from typing import TYPE_CHECKING
+
+if not TYPE_CHECKING:
+    import cesloi.event.messages
+    import cesloi.event.test
+    import cesloi.event.mirai
+    import cesloi.message.element
+    import cesloi.message
+    import cesloi.message.group
+    import cesloi.message.messageChain
