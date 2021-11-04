1b36da37e0d5b6fde8917846c7cd8c5d8b6427bc
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2021-11-04 23:36:26 +0800

    更新了个不知道是否有用的玩意

--- cesloi/delegatesystem/__init__.py
+++ cesloi/delegatesystem/__init__.py
@@ -1,6 +1,7 @@
 import asyncio
 import traceback
-from typing import List, Union, Type, Dict, Any, TYPE_CHECKING
+import time
+from typing import List, Union, Type, Dict, Any, Optional
 from .entities.event import TemplateEvent
 from .entities.publisher import Publisher
 from .entities.subsciber import SubscriberHandler
@@ -9,10 +10,14 @@ from .entities.subsciber import SubscriberHandler
 class EventDelegate:
     loop: asyncio.AbstractEventLoop
     publisher_list: List[Publisher]
+    safe_interval: float
 
-    def __init__(self):
-        self.loop = asyncio.get_event_loop()
+    def __init__(self, loop: Optional[asyncio.AbstractEventLoop] = None, interval: Optional[float] = None):
+        self.loop = loop or asyncio.get_event_loop()
         self.publisher_list = []
+        self.safe_interval = interval or 0.001
+        self.init_time = time.time()
+        self.last_time = self.init_time
 
     def parse_to_event(self, event_dict: Dict[str, Any]):
         examine_event = self.search_event(event_dict.get('type')).parse_obj(
@@ -20,11 +25,14 @@ class EventDelegate:
         return examine_event
 
     def handle_event(self, event: TemplateEvent):
-        self.loop.create_task(
-            self.exec_subscriber(
-                publisher=self.search_publisher(event.__class__),
-                event=event)
-        )
+        current_time = time.time()
+        if current_time - self.last_time > self.safe_interval:
+            self.loop.create_task(
+                self.exec_subscriber(
+                    publisher=self.search_publisher(event.__class__),
+                    event=event)
+            )
+        self.last_time = current_time
 
     async def exec_subscriber(self, publisher: Publisher, event: TemplateEvent):
         from ..event.messages import Message
@@ -40,10 +48,10 @@ class EventDelegate:
             for result in results:
                 if result.exception() == "PropagationCancelled":
                     break
-                    
+
     @staticmethod
     async def get_coroutine_executable_target(target, real_arguments, message):
-        if SubscriberHandler.command_handler(target, message):  # subscriber的command/time处理
+        if SubscriberHandler.command_handler(target, message)[0]:  # subscriber的command/time处理
             try:
                 result = await target.callable_target(**real_arguments)
             except Exception as e:
@@ -90,4 +98,5 @@ class EventDelegate:
                 else:
                     raise ValueError(f"Function \"{subscriber.name}\" for {event.__name__} has already registered!")
             return subscriber
+
         return register_wrapper
