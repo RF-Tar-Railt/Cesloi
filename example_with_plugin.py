d942c8345aeeb9fb0e8c5d2f1c9a09a6f75de28b
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2021-11-04 23:37:59 +0800

    Add files via upload

---
+++ example_with_plugin.py
@@ -0,0 +1,8 @@
+from cesloi.bot_client import Cesloi
+from cesloi.communicate_with_mah import BotSession
+
+
+bot = Cesloi(bot_session=BotSession(host="http://localhost:9080", account=2582049752, verify_key="INITKEYWylsVdbr"), debug=False)
+bot.install_plugins("test_plugins")
+bot.reload_plugins()
+bot.start()
