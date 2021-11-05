12000866b2a763880eeb2b08af70748975357854
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2021-11-06 00:32:05 +0800

    改文件的格式

--- cesloi/logger.py
+++ cesloi/logger.py
@@ -7,5 +7,6 @@ class Logger:
 
     def __init__(self, level='INFO'):
         self.logger.add('bot_running_log_' + time.strftime("%Y-%m-%d", time.localtime()) + '.log',
-                        format="{time}|{level}|{message}", level=level, enqueue=True)
+                        format="{time}|{level}|{message}", level=level, enqueue=True,
+                        rotation="500MB", encoding="utf-8")
         self.logger.info("--------------------------------------------------------------------")
