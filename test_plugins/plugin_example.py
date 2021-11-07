d73c0141864b32174ac0efe5d3c1fd9a0f90dcce
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2021-11-07 17:44:23 +0800

    Update plugin_example.py

--- test_plugins/plugin_example.py
+++ test_plugins/plugin_example.py
@@ -1,17 +1,18 @@
 from cesloi.bot_client import Cesloi
 from cesloi.event.mirai import NewFriendRequestEvent
 from cesloi.model.relation import Friend
-from cesloi.delegatesystem.entities.subsciber import SubscriberHandler
 from cesloi.message.element import Image
 from cesloi.message.messageChain import MessageChain
 from cesloi.command import Command
-from cesloi.plugin import Bellidin as bd
-
-sh = SubscriberHandler()
+from cesloi.plugin import Bellidin as bd, CommandHandler
 
 
-@bd.register("FriendMessage")
-@sh.set(command=Command(headers=["你好", "Hello"], main=["World"]))
+@bd.model_register(
+    "FriendMessage",
+    match_command=CommandHandler(
+        command=Command(headers=["你好", "Hello"], main=["World"])
+    )
+)
 async def test(app: Cesloi, friend: Friend, message: MessageChain):
     print(message.to_text())
     await app.send_with(friend, nudge=True)
@@ -19,8 +20,7 @@ async def test(app: Cesloi, friend: Friend, message: MessageChain):
     await app.send_with(friend, MessageChain([message.find(Image)]))
 
 
-@bd.register("NewFriendRequestEvent")
-@sh.set()
+@bd.model_register("NewFriendRequestEvent")
 async def test1(app: Cesloi, event: NewFriendRequestEvent):
     await event.accept()
     await app.send_friend_message(event.fromId, MessageChain.create([Image.from_local_path("test.png")]))
