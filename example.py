a8a8c492b853f0c7abf00f58501322d64f387ddb
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2021-11-11 17:30:23 +0800

    0.3.7

--- example.py
+++ example.py
@@ -5,19 +5,20 @@ from cesloi.delegatesystem.entities.subsciber import SubscriberHandler
 from cesloi.message.element import Image
 from cesloi.message.messageChain import MessageChain
 from cesloi.communicate_with_mah import BotSession
-from cesloi.command import Command
+from cesloi.alconna import Alconna, Arpamar
 
 sh = SubscriberHandler()
 bot = Cesloi(bot_session=BotSession(host="http://localhost:9080", account=2582049752, verify_key="INITKEYWylsVdbr"), debug=True)
 
 
 @bot.register("FriendMessage")
-@sh.set(command=Command(headers=["你好", "Hello"], main=["World"]))
-async def test(app: Cesloi, friend: Friend, message: MessageChain):
+@sh.set(command=Alconna(headers=["你好", "Hello"], command=" World", main_argument=Image))
+async def test(app: Cesloi, friend: Friend, message: MessageChain, arpamar: Arpamar):
     print(message.to_text())
     await app.send_with(friend, nudge=True)
     await app.send_friend_message(friend, "Hello,World!")
-    await app.send_with(friend, MessageChain([message.find(Image)]))
+    if arpamar.matched:
+        await app.send_with(friend, MessageChain.create(arpamar.main_argument))
 
 
 @bot.register("NewFriendRequestEvent")
@@ -28,4 +29,3 @@ async def test1(app: Cesloi, event: NewFriendRequestEvent):
 
 
 bot.start()
-
