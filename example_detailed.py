d7fcb90bd61f0b1ae2946981954331a7b6e1f63d
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2021-11-14 15:59:59 +0800

    0.4.1

--- example_detailed.py
+++ example_detailed.py
@@ -8,6 +8,8 @@ from cesloi.message.messageChain import MessageChain
 from cesloi.communicate_with_mah import BotSession
 from cesloi.alconna import Alconna, AnyStr, Arpamar, Option
 from cesloi.delegatesystem import EventDelegate
+from cesloi.timing.schedule import Toconada, Toconado
+from cesloi.timing.timers import EveryTimer
 
 ed = EventDelegate()
 sh = SubscriberHandler()
@@ -20,8 +22,9 @@ bot = Cesloi(
     ),
     debug=False
 )
+tot = Toconada(ed)
 
-Hello = Alconna(headers=["你好", "Hello"], command="World", main_argument=Image)
+Hello = Alconna(headers=["你好", "Hello"], command=" World", main_argument=Image)
 Weather = Alconna(headers=["cmd.", "bot"], command=f"{AnyStr}天气", options=[Option("-d", days=AnyStr)])
 
 
@@ -36,13 +39,14 @@ async def test(app: Cesloi, friend: Friend, message: MessageChain, arpamar: Arpa
 
 
 @ed.register(GroupMessage)
-@sh.set(command=Weather)
+@sh.set(command=Weather,time_schedule=Toconado(EveryTimer().every_minute()))
 async def test1(app: Cesloi, city: MessageChain, group: Group, arpamar: Arpamar):
-    city_name = arpamar.header
-    city_day = ""
-    if arpamar.has('d'):
-        city_day = arpamar.get('d')['days']
-    await app.send_group_message(group, MessageChain([Plain(city_name + city_day)]), quote=city.find("Source"))
+    if arpamar.matched:
+        city_name = arpamar.header
+        city_day = ""
+        if arpamar.has('d'):
+            city_day = arpamar.get('d')['days']
+        await app.send_group_message(group, MessageChain([Plain(city_name + city_day)]), quote=city.find("Source"))
 
 
 @ed.register(MemberLeaveEventKick)
@@ -56,6 +60,12 @@ async def test2(app: Cesloi, group: Group, target_member: Member = 'target', ope
 async def test2(app: Cesloi, event: NewFriendRequestEvent):
     await event.accept()
     await app.send_friend_message(event.fromId, MessageChain.create([Image.from_local_path("test.png")]))
+
+
+@tot.timing(EveryTimer().every_custom_seconds(5))
+async def test2():
+    await bot.send_friend_message(9876543210, "5s!")
+
 
 
 bot.start()
