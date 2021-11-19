9e5ed9a8466e6c385b2895b676108bba2905a22f
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2021-11-20 01:19:04 +0800

    Delete cesloi directory

--- cesloi/__init__.py
+++
@@ -1,7 +0,0 @@
-from typing import TYPE_CHECKING
-
-if not TYPE_CHECKING:
-    import cesloi.event.messages
-    import cesloi.event.test
-    import cesloi.event.mirai
-    import cesloi.evnet.lifecycle
