a40db448d4a3db13667559a8027012bff1bddf29
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2021-11-20 11:53:34 +0800

    v0.5.2

--- example_detailed.py
+++ example_detailed.py
@@ -1,20 +1,20 @@
-from cesloi.bot_client import Cesloi
-from cesloi.event.mirai import NewFriendRequestEvent, MemberLeaveEventKick
-from cesloi.event.messages import GroupMessage
-from cesloi.model.relation import Friend, Group, Member
-from cesloi.delegatesystem.entities.subsciber import SubscriberHandler
-from cesloi.message.element import Image, Plain
-from cesloi.message.messageChain import MessageChain
-from cesloi.communicate_with_mah import BotSession
-from cesloi.alconna import Alconna, AnyStr, Arpamar, Option
-from cesloi.delegatesystem import EventDelegate
-from cesloi.timing.schedule import Toconada, Toconado
-from cesloi.timing.timers import EveryTimer
+import asyncio
+from arclet.cesloi.bot_client import Cesloi
+from arclet.cesloi.event.mirai import NewFriendRequestEvent, MemberLeaveEventKick
+from arclet.cesloi.event.messages import GroupMessage
+from arclet.cesloi.model.relation import Friend, Group, Member
+from arclet.cesloi.message.element import Image, Plain
+from arclet.cesloi.message.messageChain import MessageChain
+from arclet.cesloi.communicate_with_mah import BotSession
+from arclet.cesloi.message.alconna import Alconna, AnyStr, Arpamar, Option, AlconnaParser
+from arclet.cesloi.timing.schedule import Toconada
+from arclet.cesloi.timing.timers import EveryTimer
+from arclet.letoderea import EventSystem
 
-ed = EventDelegate()
-sh = SubscriberHandler()
+loop = asyncio.new_event_loop()
+es = EventSystem(loop=loop)
 bot = Cesloi(
-    delegate=ed,
+    event_system=es,
     bot_session=BotSession(
         host="http://localhost:8080",
         account=1234567890,
@@ -22,14 +22,13 @@ bot = Cesloi(
     ),
     debug=False
 )
-tot = Toconada(ed)
+tot = Toconada(es)
 
 Hello = Alconna(headers=["你好", "Hello"], command=" World", main_argument=Image)
 Weather = Alconna(headers=["cmd.", "bot"], command=f"{AnyStr}天气", options=[Option("-d", days=AnyStr)])
 
 
-@ed.register("FriendMessage")
-@sh.set(command=Hello)
+@es.register("FriendMessage", decorators=[AlconnaParser(alconna=Hello)])
 async def test(app: Cesloi, friend: Friend, message: MessageChain, arpamar: Arpamar):
     print(message.to_text())
     await app.send_with(friend, nudge=True)
@@ -38,8 +37,7 @@ async def test(app: Cesloi, friend: Friend, message: MessageChain, arpamar: Arpa
         await app.send_with(friend, MessageChain.create(arpamar.main_argument))
 
 
-@ed.register(GroupMessage)
-@sh.set(command=Weather,time_schedule=Toconado(EveryTimer().every_minute()))
+@es.register(GroupMessage, decorators=[AlconnaParser(alconna=Weather)])
 async def test1(app: Cesloi, city: MessageChain, group: Group, arpamar: Arpamar):
     if arpamar.matched:
         city_name = arpamar.header
@@ -49,14 +47,12 @@ async def test1(app: Cesloi, city: MessageChain, group: Group, arpamar: Arpamar)
         await app.send_group_message(group, MessageChain([Plain(city_name + city_day)]), quote=city.find("Source"))
 
 
-@ed.register(MemberLeaveEventKick)
-@sh.set()
+@es.register(MemberLeaveEventKick)
 async def test2(app: Cesloi, group: Group, target_member: Member = 'target', operator_member: Member = 'operator'):
     await app.send_with(group, f"{target_member.name} is kicked by {operator_member.name} !")
 
 
-@ed.register("NewFriendRequestEvent")
-@sh.set()
+@es.register("NewFriendRequestEvent")
 async def test2(app: Cesloi, event: NewFriendRequestEvent):
     await event.accept()
     await app.send_friend_message(event.fromId, MessageChain.create([Image.from_local_path("test.png")]))
@@ -65,7 +61,6 @@ async def test2(app: Cesloi, event: NewFriendRequestEvent):
 @tot.timing(EveryTimer().every_custom_seconds(5))
 async def test2():
     await bot.send_friend_message(9876543210, "5s!")
-
 
 
 bot.start()
